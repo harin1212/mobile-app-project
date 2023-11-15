165165365301cead4fdb24e7f0355c24324cbe9a
hahaharin <harin2950@naver.com>
2023-11-15 21:24:12 +0900

    add : 실시간 차트 추가

---
+++ components/News/News.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, FlatList, Image } from "react-native";
+
+const News = () => {
+
+  return (
+    <>
+    <Text style={styles.topText}>실시간 차트</Text>
+    </>
+    
+)};
+
+const styles = StyleSheet.create({
+topText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginLeft: 5
+    },
+});
+
+export default News;
--- scenes/Main/Main.js
+++ scenes/Main/Main.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, ScrollView,StyleSheet } from "react-native";
 import Header from "../../components/Header/Header.js";
 import Bar from "../../components/Bar/Bar.js";
 import RealTimeChart from "../../components/RealTimeChart/RealTimeChart.js";
 import TopMenu from "../../components/TopMenu/TopMenu.js";
 import StockIndex from "../../components/StockIndex/StockIndex.js";
+import News from "../../components/News/News.js";
 function Main() {
   return (
     <View style={styles.container}>
       <Header />
-      <TopMenu />
-      <StockIndex />
-      <RealTimeChart />
+        <TopMenu />
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <StockIndex />
+        <RealTimeChart />
+        <News />
+      </ScrollView>
       <Bar />
     </View>
   );
@@ -23,6 +27,9 @@ const styles = StyleSheet.create({
     width: "100%",
     paddingTop: 10, // 여기에 원하는 간격을 설정 (10vw에 해당하는 값)
     alignItems: "center",
+  },
+  scrollViewContainer: {
+    justifyContent: 'center',
   },
 });
 
